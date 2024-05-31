c240abaa7d1d81a2e8d4f659b3b5be50e8a3dccc
Beonel <beomed.jean@gmail.com>
2024-05-31 15:43:37 +0200

    Ajouter django-cors-headers et configurer CORS

--- JeuxOlympiques/settings.py
+++ JeuxOlympiques/settings.py
@@ -50,12 +50,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://studijeuxolympiquesparis.herokuapp.com",
+    "https://www.herokucdn.com",
 ]
 
 ROOT_URLCONF = 'JeuxOlympiques.urls'
@@ -76,10 +81,7 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://studijeuxolympiquesparis.herokuapp.com",
-    "https://www.herokucdn.com",
-]
+
 
 WSGI_APPLICATION = 'JeuxOlympiques.wsgi.application'
 
